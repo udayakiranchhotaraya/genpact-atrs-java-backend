90abea0ba14ea81a8ee06541ff55aa571f71cbb8
udayakiranchhotaraya <112773247+udayakiranchhotaraya@users.noreply.github.com>
2025-11-19 09:42:19 +0530

    feat: project initial set-up

---
+++ src/main/java/com/capstone/airlineticketreservationsystem/AirlineTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.airlineticketreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/airlineticketreservationsystem/AirlineTicketReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.airlineticketreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
