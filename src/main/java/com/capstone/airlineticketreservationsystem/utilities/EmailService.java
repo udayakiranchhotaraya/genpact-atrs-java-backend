870e60b04ded6226f2895117b98ec5b97d3cf028
udayakiranchhotaraya <112773247+udayakiranchhotaraya@users.noreply.github.com>
2025-11-28 02:36:06 +0530

    feat: email utility service added with sendHtmlVerificationEmail method
    Signed-off-by: udayakiranchhotaraya <112773247+udayakiranchhotaraya@users.noreply.github.com>

---
+++ src/main/java/com/capstone/airlineticketreservationsystem/utilities/EmailService.java
@@ -0,0 +1,32 @@
+package com.capstone.airlineticketreservationsystem.utilities;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendHtmlVerificationEmail(String toEmail, String verificationUrl) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom("genpactcapstoneatrs2025@gmail.com");
+        helper.setTo(toEmail);
+        helper.setSubject("Verify Your Account for Airline Ticket Reservation System");
+
+        String htmlContent = """
+                <h3>Welcome to the Airline Ticket Reservation System</h3>
+                <p>Thank you for registering with us. To complete your account setup, please create your password using the link below.</p>
+                <p><a href="%s">Set Your Password</a></p>
+                <p>If you did not initiate this registration, please ignore this email.</p>
+            """.formatted(verificationUrl);
+        helper.setText(htmlContent, true); // 'true' indicates the text is HTML
+
+        mailSender.send(message);
+    }
+}
